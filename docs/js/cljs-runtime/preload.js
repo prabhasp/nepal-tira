a553190b6f1e5837df122ccac61a15b8a864bc70
Prabhas Pokharel <prabhas.pokharel@gmail.com>
2024-03-23 19:03:06 -0400

    clear out public + docs for a second.

--- docs/js/cljs-runtime/preload.js
+++
@@ -1,7 +0,0 @@
-goog.provide('preload');
-uix.dev.init_fast_refresh_BANG_();
-preload.refresh = (function preload$refresh(){
-return uix.dev.refresh_BANG_();
-});
-
-//# sourceMappingURL=preload.js.map
